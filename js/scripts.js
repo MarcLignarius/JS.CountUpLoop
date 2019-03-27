9e39121a3c402c73882817637178cfc85b3a1091
Kerriann Walker and Marc Davies <kerriann@>
2019-03-27 14:59:07 -0700

    add functionality to our page. 4 Calculators, user input is front end and back end functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
-var number1 = parseInt(prompt("Enter a number: "));
-var number2 = parseInt(prompt("Enter another number: "));
+
 
 var add = function(number1, number2) {
 	return number1 + number2;
@@ -14,7 +13,36 @@ var divide = function(number1, number2) {
 	return number1 / number2;
 };
 
-alert("The result of " + number1 + " plus " +  number2 + " is " + add(number1, number2));
-alert("The result of " + number1 + " minus " +  number2 + " is " + subtract(number1, number2));
+$(document).ready(function() {
+	$("#add").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("#add1").val());
+		var number2 = parseInt($("#add2").val());
+		var result = add(number1, number2);
+		$("#output").text(result);
+	});
+	$("#subtract").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("#subtract1").val());
+		var number2 = parseInt($("#subtract2").val());
+		var result = subtract(number1, number2);
+		$("#output2").text(result);
+	});
+	$("#multiply").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("#multiply1").val());
+		var number2 = parseInt($("#multiply2").val());
+		var result = multiply(number1, number2);
+		$("#output3").text(result);
+	});
+	$("#divide").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("#divide1").val());
+		var number2 = parseInt($("#divide2").val());
+		var result = divide(number1, number2);
+		$("#output4").text(result);
+	});
+});
+/*alert("The result of " + number1 + " minus " +  number2 + " is " + subtract(number1, number2));
 alert("The result of " + number1 + " multiplied by " +  number2 + " is " + multiply(number1, number2));
-alert("The result of " + number1 + " divided by " +  number2 + " is " + divide(number1, number2));
+alert("The result of " + number1 + " divided by " +  number2 + " is " + divide(number1, number2));*/
