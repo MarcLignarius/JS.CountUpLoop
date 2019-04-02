0ae59d9433300b6576d02361f58d3ae00c48fa01
Liz Kelley and Marc Davies <marc@>
2019-04-02 11:44:35 -0700

    add js logic to prevent user from entering 0 or negative numbers, also changed input type in html to number to prevent user from entering text

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,18 @@
-$(document).ready(function() {
-
-	$("#countup").submit(function(event){
+$(function() {
+	$("#countup").submit(function(event) {
 		event.preventDefault();
 		var number1 = parseInt($("#countTo").val());
 		var number2 = parseInt($("#countBy").val());
 		console.log (number1);
 		console.log (number2);
 
-		var total = 0;
-		for(var index = 0; index <= number1; index += number2) {
-			alert(index);
+		if((number1 >= 0 || number2 >= 0) || (number1 >= 0 && number2 >= 0)) {
+				$("#output").text("Please enter numbers greater than zero.");
+			} else {
+				var total = 0;
+				for(var index = 0; index <= number1; index += number2) {
+				var result = $("#output").append(index + ", ");
+			};
 		}
 	});
 });
