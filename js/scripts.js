114ccde69a949f7808cdc191d9234957284ae9b7
Marc Davies <marcdaviesriot@gmail.com>
2020-01-14 23:25:15 -0800

    check for empty fields rather than isNaN

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,16 @@
 $(function() {
 	$("#countUp").submit(function(event) {
 		event.preventDefault();
-		let userInput1 = ($("#userInput1").val());
-		let userInput2 = ($("#userInput2").val());
-		console.log('userInput1', userInput1);
-		console.log('userInput2', userInput2);
-		if (isNaN(userInput1) || isNaN(userInput2)) {
-			alert("Please enter a value in both fields.");
-			return false;
-		} else {
-			let countTo = parseInt(userInput1);
-			let countBy = parseInt(userInput2);
-			console.log('countTo', countTo);
-			console.log('countBy', countBy)
+		let userInput1 = $("#userInput1").val();
+		let userInput2 = $("#userInput2").val();
+		let form = document.getElementById("countUp");
+		form.reset();	
+		//Check for empty fields
+		if (userInput1 == "" || userInput2 == "") {
+			alert("Both fields are required.");
+			} else {
+				alert('All good');	
+			}
 		}
-	});
-})
+	);
+});
