2663982d52f9fecd0b2858ad1d6fe9e6fa1fb151
Marc Davies <marcdaviesriot@gmail.com>
2020-01-14 21:30:58 -0800

    remove obsolete code and comment out current script before rework

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,17 @@
-$(function() {
-	$("#countUp").submit(function(event) {
-		event.preventDefault();
-		var number1 = parseInt($("#countTo").val());
-		var number2 = parseInt($("#countBy").val());
-		if((number1 === 0 || number2 === 0) || (number1 === 0 && number2 === 0) || (number2 > number1)) {
-				$("#output").text("Count to number must be greater than Count by number and both must be greater than 0.")
-			}	else {
-				var total = 0;
-				for(var index = 0; index <= number1; index += number2) {
-				var result = $("#output").append(index + ", ");
-			};
-		}
-	});
-});
 
 
-// if(number2 > number1) {
-// 		$("#output").text("Count To number must be greater than Count By number.");
-// 	} else if((number1 >= 0 || number2 >= 0) || (number1 >= 0 && number2 >= 0)) {
-// 		$("#output").text("Please enter numbers greater than zero.");
-// 	}	else {
-// 		var total = 0;
-// 		for(var index = 0; index <= number1; index += number2) {
-// 		var result = $("#output").append(index + ", ");
-// 	};
-// }
-// });
+// $(function() {
+// 	$("#countUp").submit(function(event) {
+// 		event.preventDefault();
+// 		var number1 = parseInt($("#countTo").val());
+// 		var number2 = parseInt($("#countBy").val());
+// 		if((number1 === 0 || number2 === 0) || (number1 === 0 && number2 === 0) || (number2 > number1)) {
+// 				$("#output").text("Count to number must be greater than Count by number and both must be greater than 0.")
+// 			}	else {
+// 				var total = 0;
+// 				for(var index = 0; index <= number1; index += number2) {
+// 				var result = $("#output").append(index + ", ");
+// 			};
+// 		}
+// 	});
 // });
