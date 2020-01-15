2adf0b320ef451c23bdbca5ba71ebdba42d5febe
Marc Davies <marcdaviesriot@gmail.com>
2020-01-14 23:44:54 -0800

    clear form fields on submit

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,9 @@ $(function() {
 	$("#countUp").submit(function(event) {
 		event.preventDefault();
 		let userInput1 = $("#userInput1").val();
-		let userInput2 = $("#userInput2").val();
+    let userInput2 = $("#userInput2").val();
+    let form = document.getElementById("countUp");
+		form.reset();	
 		//Check for empty fields
 		if (userInput1 == "" || userInput2 == "") {
 			alert("Both fields are required.");
