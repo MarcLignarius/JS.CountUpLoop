1f82292296e0e1958f2fe7e8471478bfe0494c3b
Marc Davies <marcdaviesriot@gmail.com>
2020-01-14 23:28:34 -0800

    successfully check for non-numerical values

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,9 @@ $(function() {
 		//Check for empty fields
 		if (userInput1 == "" || userInput2 == "") {
 			alert("Both fields are required.");
+			//Check for non-numerical values
+			} else if (isNaN(userInput1) || isNaN(userInput2)) {
+				alert("Numbers only please.");
 			} else {
 				alert('All good');	
 			}
