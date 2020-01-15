7363421f1aebcd3fd0df7fed21971f6e35064b05
Marc Davies <marcdaviesriot@gmail.com>
2020-01-14 21:42:41 -0800

    retrieve user input and save it as number1 and number 2 and display it to the console

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,9 @@
-
-
-// $(function() {
-// 	$("#countUp").submit(function(event) {
-// 		event.preventDefault();
-// 		var number1 = parseInt($("#countTo").val());
-// 		var number2 = parseInt($("#countBy").val());
-// 		if((number1 === 0 || number2 === 0) || (number1 === 0 && number2 === 0) || (number2 > number1)) {
-// 				$("#output").text("Count to number must be greater than Count by number and both must be greater than 0.")
-// 			}	else {
-// 				var total = 0;
-// 				for(var index = 0; index <= number1; index += number2) {
-// 				var result = $("#output").append(index + ", ");
-// 			};
-// 		}
-// 	});
-// });
+$(function() {
+	$("#countUp").submit(function(event) {
+	event.preventDefault();
+	let number1 = parseInt($("#countTo").val());
+	let number2 = parseInt($("#countBy").val());
+	console.log('number1', number1);
+	console.log('number2', number2);
+	});
+});
