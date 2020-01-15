f279e87d55a630eb1ff548e3abcb84cf986aeef2
Marc Davies <marcdaviesriot@gmail.com>
2020-01-14 23:26:08 -0800

    remove unused code

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,6 @@ $(function() {
 		event.preventDefault();
 		let userInput1 = $("#userInput1").val();
 		let userInput2 = $("#userInput2").val();
-		let form = document.getElementById("countUp");
-		form.reset();	
 		//Check for empty fields
 		if (userInput1 == "" || userInput2 == "") {
 			alert("Both fields are required.");
