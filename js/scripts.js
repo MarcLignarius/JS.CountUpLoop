c16f5f0f869237c2bed7479561f441d72195ca42
Marc Davies <marcdaviesriot@gmail.com>
2020-01-14 22:13:23 -0800

    successfully check for empty fields

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
 $(function() {
 	$("#countUp").submit(function(event) {
-	event.preventDefault();
-	let number1 = parseInt($("#countTo").val());
-	let number2 = parseInt($("#countBy").val());
-	console.log('number1', number1);
-	console.log('number2', number2);
+		event.preventDefault();
+		let userInput1 = ($("#userInput1").val());
+		let userInput2 = ($("#userInput2").val());
+		console.log('userInput1', userInput1);
+		console.log('userInput2', userInput2);
+		if (isNaN(userInput1) || isNaN(userInput2)) {
+			alert("Please enter a value in both fields.");
+			return false;
+		} else {
+			let countTo = parseInt(userInput1);
+			let countBy = parseInt(userInput2);
+			console.log('countTo', countTo);
+			console.log('countBy', countBy)
+		}
 	});
-});
+})
