5bd692e20105be4a286b72effee2d24ca99203b0
Marc Davies <marcdaviesriot@gmail.com>
2020-01-15 17:03:39 -0800

    display messages in output div rather than alerts

--- js/scripts.js
+++ js/scripts.js
@@ -7,29 +7,28 @@ $(function() {
     form.reset();
     //Check for empty fields
     if (userInput1 == "" || userInput2 == "") {
-      alert("Both fields are required.");
+      $("#output").text("Both fields are required.");
       //Check for non-numerical values
     } else if (isNaN(userInput1) || isNaN(userInput2)) {
-      alert("Numbers only please.");
+      $("#output").text("Numbers only please.");
     } else {
       //If non-empty and numerical, parse user input
       let countTo = parseFloat(userInput1);
       let countBy = parseFloat(userInput2);
       //Check for negative numbers
       if (countTo < 0 || countBy < 0) {
-        alert("Positive numbers only please.");
+        $("#output").text("Positive numbers only please.");
         //Check for non-whole numbers
       } else if (countTo % 1 !== 0 || countBy % 1 !== 0) {
-        alert("Let's stick to whole numbers shall we?");
+        $("#output").text("Let's stick to whole numbers shall we?");
       } else if (countBy > countTo) {
         //Check that countBy is not higher than countTo
-        alert("Count to number must be higher than count by number.");
+        $("#output").text("Count to number must be higher than count by number.");
       } else {
         //Count up to countTo number by adding up countBy
         let result = [];
         for (let index = 0; index <= countTo; index += countBy) {
           result.push(" " + index);
-          console.log(result);
           $("#output").text(result);
         }
       }
